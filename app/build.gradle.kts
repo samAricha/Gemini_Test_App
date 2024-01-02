d946c43a18136b1e258ded86bac89acae001b6bc
samAricha <samaricha001@gmail.com>
2024-01-02 09:08:14 +0300

    added gemini and coil dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,4 +66,8 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+
+    implementation("com.google.ai.client.generativeai:generativeai:0.1.1")
+    implementation("io.coil-kt:coil-compose:2.5.0")
 }
